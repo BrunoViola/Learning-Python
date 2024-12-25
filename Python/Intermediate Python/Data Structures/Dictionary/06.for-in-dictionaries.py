b048c359c51d12153acbd26a669858475da29fd2
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-12-24 23:02:53 -0300

    add: usando o for para iterar sobre um dicionário [Intermediate Python]

---
+++ Python/Intermediate Python/Data Structures/Dictionary/06.for-in-dictionaries.py
@@ -0,0 +1,8 @@
+world = {
+   "Afghanistan": 30.55,
+   "Albania": 2.77,
+   "Algeria": 39.21
+}
+
+for key, value in world.items():
+   print(f'Country {key} has a population of {value} million people')
